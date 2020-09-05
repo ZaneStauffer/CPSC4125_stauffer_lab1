29d01f82e98978041f6360cfddddb7a2295cd080
ZaneStaufer <stauffer_zane@columbusstate.edu>
2020-09-05 19:54:17 -0400

    Add hover li functionality

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(() => {
+    // Entry hover functionality
+    $("li").hover(function(){
+        $(this).attr('class', 'list-group-item active');
+        $(this).find('button').show();
+    }, function(){
+        $(this).attr('class', 'list-group-item');
+        $(this).find('button').hide();
+    });
     
 });
