124186375b4988a92318743a09ceabcdad8c12e5
ZaneStaufer <stauffer_zane@columbusstate.edu>
2020-09-05 22:10:35 -0400

    Change template skeleton

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// We define a template for the entry. Normally I would never do it like this, but CORS prevents loading templates on the local machine.
-var entry_template = `
-<li class="list-group-item">
-    <div class="row">
-        <div class="col-sm-11">
-            <div class="contents">
-                <p style="margin: 0 0 0 0;">Enter your text here!</p>
-            </div>
-        </div>
-        <div class="col-sm">
-            <div class="float-right" style="margin: auto;">
-                <!-- The button is hidden until the mouse is hovered over the entry. -->
-                <button type="button" class="btn btn-default" style="padding: 0; visibility: hidden;">
-                    <i class="fa fa-trash fa-2x"></i>
-                </button>
-            </div>
-        </div>
-    </div>
-</li>
-`;
 $(document).ready(() => {
     // Entry hover functionality
     $(document).on("mouseenter","li", function(){
@@ -36,6 +16,6 @@ $(document).ready(() => {
 
     // Add button functionality
     $('#add-button').click(function(){
-        $('#todo').append($.parseHTML(entry_template));
+        $('#todo').append($('#entry-template').html());
     });
 });
